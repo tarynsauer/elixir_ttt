d1cb9b6aa83c32264f335b5d7bbdbf65f9d47668
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-26 16:59:31 -0500

    Adds method to create Player struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,3 +1,8 @@
 defmodule Player do
+  defstruct marker: "X", type: "human"
+
+  def create_player(marker, type) do
+    %Player{marker: marker, type: type}
+  end
 
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -2,4 +2,10 @@ defmodule PlayerTest do
   use ExUnit.Case
   import Player
 
+  test "creates a new player" do
+    player = create_player("X", "human") 
+    assert player.marker == "X" 
+    assert player.type == "human"
+  end
+
 end
