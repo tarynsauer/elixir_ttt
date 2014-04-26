673db03464b026dc4bf860c8a65cf2073454cba2
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-26 17:00:40 -0500

    Adds print board function

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,7 +1,7 @@
 defmodule Ui do
 
-  def print_message(message) do
-    IO.puts message
+  def print_line(text) do
+    IO.puts text 
   end
 
   def get_input do
@@ -10,7 +10,21 @@ defmodule Ui do
   end
 
   def request_player_type(marker) do
-    print_message "Enter player type for #{marker}:"
+    print_line "Enter player type for #{marker}:"
   end
 
+  def print_row(row) do
+    print_line Enum.join(row, " ")
+  end
+
+  def get_row(board, first, last) do
+    range = Range.new(first, last)
+    Enum.slice(board, range)
+  end
+
+  def print_board(board) do
+    print_row get_row(board, 0, 2) 
+    print_row get_row(board, 3, 5)
+    print_row get_row(board, 6, 8) 
+  end
 end
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -16,4 +16,10 @@ defmodule UiTest do
     end) == "human"
   end
 
+  test "prints board" do
+     assert capture_io(fn ->
+       print_board(["X", 2, 3, 4, 5, 6, 7, 8, 9])
+     end) == "X 2 3\n4 5 6\n7 8 9\n" 
+  end
+
 end
