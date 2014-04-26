fd15791b6e4590266574b4fb8642ef413921b7af
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-26 17:01:34 -0500

    Add Game namespace

---
+++ test/game_test.exs
@@ -0,0 +1,10 @@
+defmodule GameTest do
+  use ExUnit.Case
+  import Game 
+
+  # test "creates a new board" do
+  #   board = create_board()
+  #   assert board == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  # end
+
+end
