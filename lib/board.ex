4f997c60b4bda6506d5e28b649e0491f947543ed
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 15:15:08 -0500

    Add game_over/board status methods

--- lib/board.ex
+++ lib/board.ex
@@ -5,6 +5,20 @@ defmodule Board do
     @standard_board
   end
 
+  def get_row(board, first, last) do
+    range = Range.new(first, last)
+    Enum.slice(board, range)
+  end
+
+  def get_col(board, first, last) do
+    middle = first + 3
+    [Enum.at(board, first), Enum.at(board, middle), Enum.at(board, last)]
+  end
+
+  def get_diag(board, first, last) do
+    [Enum.at(board, first), Enum.at(board, 4), Enum.at(board, last)]
+  end
+
   def open_cell?(board, cell_index) do
     cell_value = Enum.at(board, cell_index)
     is_integer(cell_value)
@@ -12,6 +26,37 @@ defmodule Board do
 
   def add_marker(board, marker, cell_index) do
     List.replace_at(board, cell_index, marker)
+  end
+
+  def board_full?(board) do
+    open_cells = Enum.filter(board, fn(x) -> (x != "X") && (x != "O") end)
+    Enum.empty?(open_cells)
+  end
+
+  def winning_game?(board) do
+    winning_row?(board) || winning_col?(board) || winning_diag?(board)
+  end
+
+  def winning_row?(board) do
+    winning_line?(get_row(board, 0, 2)) || winning_line?(get_row(board, 3, 5)) ||
+  winning_line?(get_row(board, 6, 8))
+  end
+
+  def winning_col?(board) do
+    winning_line?(get_col(board, 0, 6)) || winning_line?(get_col(board, 1, 7))
+    || winning_line?(get_col(board, 2, 9))
+  end
+
+  def winning_diag?(board) do
+    winning_line?(get_diag(board, 0, 8)) || winning_line?(get_diag(board, 2, 6))
+  end
+
+  def winning_line?(line) do
+    (length Enum.uniq(line)) == 1
+  end
+
+  def game_over?(board) do
+    winning_game?(board) || board_full?(board)    
   end
 
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -7,6 +7,11 @@ defmodule BoardTest do
     assert board == [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
+  test "gets column values" do
+    board = new_board
+    assert get_col(board, 0, 6) == [1, 4, 7]
+  end
+
   test "indicates whether cell is open" do
     board = ["X", 2, 3, 4, 5, 6, 7, 8, 9]
     refute open_cell?(board, 0)
@@ -15,6 +20,55 @@ defmodule BoardTest do
   test "adds marker to the board" do
     board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert add_marker(board, "X", 0) == ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  test "returns true, indicates no moves remaining" do
+    board = ["X", "O", "X", 
+             "X", "O", "O", 
+             "O", "X", "X"]
+    assert board_full?(board)
+  end
+
+  test "returns true when a player has a winning row" do
+    board = [1, 2, 3, 
+             "O", "O", 6, 
+             "X", "X", "X"]
+    assert winning_game?(board)
+  end
+
+  test "returns true when a player has a winning column" do
+    board = ["X", 2, 3, 
+             "X", "O", 6, 
+             "X", "O", 9]
+    assert winning_game?(board)
+  end
+
+  test "returns true when a play has a winning diagonal line" do
+    board = ["O", 2, 3, 
+             "X", "O", 6, 
+             "X", "X", "O"]
+    assert winning_game?(board)
+  end
+
+  test "indicates game is over for win" do
+    board = ["O", 2, 3, 
+             "X", "O", 6, 
+             "X", "X", "O"]
+    assert game_over?(board)
+  end
+
+  test "indicates game is over for tie" do
+    board = ["X", "O", "X", 
+             "X", "O", "O", 
+             "O", "X", "X"]
+    assert game_over?(board)
+  end
+
+  test "indicates game is not over" do
+    board = ["X", 2, "X", 
+             4, "O", "O", 
+             "O", "X", "X"]
+    refute game_over?(board)
   end
 
 end
