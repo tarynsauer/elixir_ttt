a8cf1190d0ae7d153ed5b6f28fcf8b5686b92a73
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 00:58:24 -0500

    Add current_player function

--- lib/game.ex
+++ lib/game.ex
@@ -1,3 +1,23 @@
 defmodule Game do
+  import Integer
+  import Board
+  
+  @x_marker "X"
+  @o_marker "O"
+  # def new_game do
+  #   board = create_board  
+  #   player_x = get_player(:X)
+  #   player_o = get_player(:O)
+  #   play(board, player_x, player_o)
+  # end
+  #
+  # def play(board, player_x, player_o) do
+  #
+  # end
+
+  def current_player(board) do
+    x_count = Enum.filter(board, fn(x) -> x == @x_marker end)
+    if even?(length x_count), do: @x_marker, else: @o_marker 
+  end
 
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -2,9 +2,9 @@ defmodule GameTest do
   use ExUnit.Case
   import Game 
 
-  # test "creates a new board" do
-  #   board = create_board()
-  #   assert board == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  # end
+  test "returns the marker of the current player" do
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert current_player(board) == "X"
+  end
 
 end
