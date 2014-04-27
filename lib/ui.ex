4a0d117ff608501dadec377a63287e0fd2b86eb0
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 00:59:45 -0500

    Add validation message functions

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,5 +1,5 @@
 defmodule Ui do
-
+  
   def print_line(text) do
     IO.puts text 
   end
@@ -11,6 +11,20 @@ defmodule Ui do
 
   def request_player_type(marker) do
     print_line "Enter player type for #{marker}:"
+  end
+
+  def request_player_move(marker) do
+    print_line "Player #{marker}, make your move:"
+  end
+
+  def invalid_type_message(marker) do
+    print_line "Invalid type."
+    request_player_type(marker)
+  end
+
+  def invalid_move_message(marker) do
+    print_line "Invalid move."
+    request_player_move(marker)
   end
 
   def print_row(row) do
@@ -27,4 +41,5 @@ defmodule Ui do
     print_row get_row(board, 3, 5)
     print_row get_row(board, 6, 8) 
   end
+  
 end
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -9,11 +9,29 @@ defmodule UiTest do
      end) == "Enter player type for X:\n" 
   end
 
+  test "requests player move" do
+    assert capture_io(fn ->
+       request_player_move("X")
+     end) == "Player X, make your move:\n" 
+  end
+
   test "normalizes player type input" do
     assert capture_io([input: "Human", capture_prompt: false], fn ->
       input = get_input 
       IO.write input
     end) == "human"
+  end
+
+  test "prints invalid type message" do
+     assert capture_io(fn ->
+       invalid_type_message("X")
+     end) == "Invalid type.\nEnter player type for X:\n" 
+  end
+
+  test "prints invalid move message" do
+     assert capture_io(fn ->
+       invalid_move_message("X")
+     end) == "Invalid move.\nPlayer X, make your move:\n" 
   end
 
   test "prints board" do
