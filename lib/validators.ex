187996067b16ef8e7a98b72b2811d30718188f49
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 00:56:52 -0500

    Namespaced all validation-related functions

---
+++ lib/validators.ex
@@ -0,0 +1,63 @@
+defmodule Validators do
+  import Player
+  import Ui
+  import Board, only: [open_cell?: 2, add_marker: 3]
+
+  @valid_types player_types
+
+  def valid_type?(type) do
+    type = String.downcase(type)
+    Enum.any?(@valid_types, fn(x) -> x == type end)
+  end
+
+  def valid_cell_index?(board, cell_index) do
+    (0 <= cell_index) && (cell_index < 10) && open_cell?(board, cell_index)
+  end
+
+  def valid_move?(board, move) do
+    valid_integer?(move) && valid_cell_index?(board, get_cell_index(move))
+  end
+
+  def valid_integer?(move) do
+    try do 
+       is_tuple(Integer.parse(move))
+    end
+  end
+
+  def get_cell_index(move) do
+    parsed_move = Integer.parse(move)
+    elem(parsed_move, 0) - 1
+  end
+
+  def validate_move(board, move, marker) do
+    if valid_move?(board, move) do
+      cell_index = get_cell_index(move)
+      add_marker(board, marker, cell_index) 
+    else
+      invalid_move_message(marker)
+      request_player_move(marker)
+    end
+  end
+
+  def validate_type(type, marker) do
+    if valid_type?(type) do
+      create_player(marker, type)
+    else
+      invalid_type_message(marker)
+      get_player(marker)
+    end
+  end
+
+  def get_player(marker) do
+    request_player_type(marker)
+    type = get_input
+    validate_type(type, marker)
+  end
+
+  def get_move(board, marker) do
+    request_player_move(marker)
+    move = get_input
+    validate_move(board, move, marker)
+  end
+
+end
---
+++ test/validators_test.exs
@@ -0,0 +1,29 @@
+defmodule ValidatorsTest do
+  use ExUnit.Case
+  import Validators   
+
+  test "creates player when type is valid" do
+    assert validate_type("human", "X") == %Player{marker: "X", type: "human"}
+  end
+
+  test "returns true if move is valid" do
+    assert valid_move?([1, 2, 3, 4, 5, 6, 7, 8, 9], "1")
+  end
+
+  test "returns false if move is invalid" do
+    refute valid_move?([1, 2, 3, 4, 5, 6, 7, 8, 9], "G")
+  end
+
+  test "returns false if board move occupied" do
+    refute valid_move?(["X", 2, 3, 4, 5, 6, 7, 8, 9], "1")
+  end
+
+  # test "creates player for valid type input" do
+  #   output = capture_io([input: "human", capture_prompt: false], fn ->
+  #     get_player("X") 
+  #   end) 
+  #   assert output == %Player{marker: "X", type: "human"}
+  # end
+
+  
+end
