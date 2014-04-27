4fccddcde555b5325dc2efd841eea77255f56397
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 00:58:59 -0500

    Add player_types attributes method

--- lib/player.ex
+++ lib/player.ex
@@ -1,8 +1,16 @@
 defmodule Player do
-  defstruct marker: "X", type: "human"
+  @human_player "human"
+  @computer_player "computer"
+  @ai_player "ai"
+
+  defstruct marker: "X", type: @human_player 
 
   def create_player(marker, type) do
     %Player{marker: marker, type: type}
+  end
+
+  def player_types do
+    [@human_player, @computer_player, @ai_player]
   end
 
 end
