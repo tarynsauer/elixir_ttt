3840ed1baf9ee725fa26a456f12fdeeea4237e93
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-27 15:16:14 -0500

    Update project file for escriptizing

--- mix.exs
+++ mix.exs
@@ -5,20 +5,19 @@ defmodule ElixirTtt.Mixfile do
     [app: :elixir_ttt,
      version: "0.0.1",
      elixir: "~> 0.13.0",
-     deps: deps]
+     escript_main_module: Run,
+     # escript_path: "run",
+     deps: deps,
+     compile_path: "ebin"]
   end
 
-  # Configuration for the OTP application
-  #
   # Type `mix help compile.app` for more information
   def application do
-    [ applications: [] ]
+     [ applications: [] ]
   end
 
   # List all dependencies in the format:
-  #
   # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
   # Type `mix help deps` for more examples and options
   defp deps do
     []
