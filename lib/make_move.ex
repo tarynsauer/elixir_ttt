7b8916549418cf67b5ff0c259b7726be2c853a5f
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-28 10:57:38 -0500

    Fix arguments to MakeMove implementations

--- lib/make_move.ex
+++ lib/make_move.ex
@@ -1,5 +1,6 @@
 import Ui, only: [get_input: 0]
-import Validators, only: [validate_move, 3]
+import Validators, only: [validate_move: 3]
+import ComputerPlayer
 
 defprotocol MakeMove do
   @doc "Returns cell index of player move"
@@ -8,18 +9,18 @@ defprotocol MakeMove do
 end
 
 defimpl MakeMove, for: HumanPlayer do
-  def make_move(_, board) do
+  def make_move(player, board) do
     move = get_input 
-    #validate_move(board, move, player.marker) 
+    validate_move(board, move, player.marker) 
   end
 end
 
 defimpl MakeMove, for: ComputerPlayer do
-  def make_move(_, board) do 
-    # make_random_move(board, player.marker)
+  def make_move(player, board) do 
+    make_random_move(board, player.marker)
   end
 end
 
 defimpl MakeMove, for: AiPlayer do
-  def make_move(_, board), do: "ai move" 
+  def make_move(player, board), do: "ai move" 
 end
