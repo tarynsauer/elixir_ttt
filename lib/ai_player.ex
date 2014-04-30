98776928f4d3b23525891c466a986e0bee7fddbb
Taryn Sauer <taryn.sauer@gmail.com>
2014-04-30 14:21:17 -0500

    WIP: Alphabeta depth not incrementing

--- lib/ai_player.ex
+++ lib/ai_player.ex
@@ -26,31 +26,55 @@ defmodule AiPlayer do
   end
 
   def move_score(board, ai_marker, cell) do
-    apply_minimax(add_marker(board, cell), ai_marker,
-    ai_marker, 1)     
+    apply_minimax(add_marker(board, cell), ai_marker, ai_marker, 1, [0, -999, 999])     
   end
 
-  def minimax(board, ai_marker, marker, depth) do
-    cells = open_cells(board)
-    Enum.map(cells, fn cell -> (apply_minimax(add_marker(board,
-    cell), ai_marker, current_player_marker(board), (depth + 1)) / depth) end)     
+  def alphabeta(board, ai_marker, marker, depth, values) do
+    Enum.flat_map_reduce(board, values, fn cell, acc ->
+      if no_break?(acc) do
+        apply_minimax(add_marker(board, cell),
+        ai_marker, current_player_marker(board), depth, acc)
+      else
+        {:halt, acc}  ##need to return score,
+      end
+    end)
   end
 
-  def apply_minimax(board, ai_marker, marker, depth) do
+  def apply_minimax(board, ai_marker, marker, depth, acc) do
     if game_over?(board) do
       score(board, ai_marker)
     else
-      scores = minimax(board, ai_marker, marker, depth)
-      if ai_marker == marker do
-        Enum.min(scores)
-      else
-        Enum.max(scores)
-      end
+      score = alphabeta(board, ai_marker, current_player_marker(board), (depth + 1), acc)
+      alpha = get_alpha(ai_marker, marker, acc, score)
+      beta = get_beta(ai_marker, marker, acc, score)
+      [score, alpha, beta]
+    end
+  end
+
+  def no_break?(acc) do
+    Enum.at(acc, 2) < Enum.at(acc, 1)
+  end
+
+  def get_alpha(ai_marker, marker, acc, score) do
+    alpha = Enum.at(acc, 1)
+    if marker == ai_marker do  ### Might be opposite
+      alpha
+    else
+      if score > alpha, do: score, else: alpha
+    end
+  end
+
+  def get_beta(ai_marker, marker, acc, score) do
+    beta = Enum.at(acc, 2)
+    if marker == ai_marker do  ### Might be opposite
+      if score < beta, do: score, else: beta
+    else
+      beta
     end
   end
 
   def ai_player_win?(board, ai_marker) do
-   current_player_marker(board) != ai_marker
+    current_player_marker(board) != ai_marker
   end
 
   def score(board, ai_marker) do
@@ -60,5 +84,4 @@ defmodule AiPlayer do
       @tie
     end
   end
-
 end
