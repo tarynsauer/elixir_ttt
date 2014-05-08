c863fb586e21521d65ae527c700aa78cf2763e86
Taryn Sauer <taryn.sauer@gmail.com>
2014-05-08 14:51:20 -0500

    Reorder functions for clarity

--- lib/validators.ex
+++ lib/validators.ex
@@ -4,40 +4,13 @@ defmodule Validators do
   import Board, only: [new_board: 1, open_cell?: 2, add_marker: 2]
   import GameUtils
 
-  def valid_type?(type) do
-    type = String.downcase(type)
-    Enum.any?(player_types, fn(x) -> x == type end)
-  end
-
-  def within_range?(board, cell_index) do
-    (cell_index >= 0) && (cell_index < (length board))
-  end
-
-  def valid_cell_index?(board, cell_index) do
-    within_range?(board, cell_index) && open_cell?(board, cell_index)
-  end
-
-  def valid_move?(board, move) do
-    valid_integer?(move) && valid_cell_index?(board, get_cell_index(move))
-  end
-
-  def valid_size?(size) do
-    parsed_size = parse_integer(size)
-    (3 <= parsed_size) && (parsed_size <= 6)
-  end
-
-  def valid_board_size?(size) do
-    valid_integer?(size) && valid_size?(size)
-  end
-
-  def valid_integer?(move) do
-    try do 
-      is_tuple(Integer.parse(move))
+  def validate_move(board, move, marker) do
+    if valid_move?(board, move) do
+      play_valid_move(board, move)
+    else
+      handle_invalid_move(marker)
+      board
     end
-  end
-
-  def get_cell_index(move) do
-    parse_integer(move) - 1
   end
 
   def play_valid_move(board, move) do
@@ -48,15 +21,6 @@ defmodule Validators do
   def handle_invalid_move(marker) do
     invalid_move_message
     request_player_move(marker)
-  end
-
-  def validate_move(board, move, marker) do
-    if valid_move?(board, move) do
-      play_valid_move(board, move)
-    else
-      handle_invalid_move(marker)
-      board
-    end
   end
 
   def validate_type(type, marker) do
@@ -92,6 +56,42 @@ defmodule Validators do
     request_player_move(marker)
     move = get_input
     validate_move(board, move, marker)
+  end
+
+  def valid_type?(type) do
+    type = String.downcase(type)
+    Enum.any?(player_types, fn(x) -> x == type end)
+  end
+
+  def within_range?(board, cell_index) do
+    (cell_index >= 0) && (cell_index < (length board))
+  end
+
+  def valid_cell_index?(board, cell_index) do
+    within_range?(board, cell_index) && open_cell?(board, cell_index)
+  end
+
+  def valid_move?(board, move) do
+    valid_integer?(move) && valid_cell_index?(board, get_cell_index(move))
+  end
+
+  def valid_size?(size) do
+    parsed_size = parse_integer(size)
+    (3 <= parsed_size) && (parsed_size <= 6)
+  end
+
+  def valid_board_size?(size) do
+    valid_integer?(size) && valid_size?(size)
+  end
+
+  def valid_integer?(move) do
+    try do 
+      is_tuple(Integer.parse(move))
+    end
+  end
+
+  def get_cell_index(move) do
+    parse_integer(move) - 1
   end
 
 end
